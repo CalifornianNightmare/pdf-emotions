74102a70a02237c425414d9642f7642cdff77566
CalifornianNightmare <egbezverh@gmail.com>
2025-07-05 00:21:28 +0300

    add: try-except security layer to pdf reader

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ These instructions will get you a copy of the project up and running on your loc
 ### Prerequisites
 
 - Python 3.12.3
-- pip
+- Pip
 
 ### Installing
 
--- app/pdf/reader.py
+++ app/pdf/reader.py
@@ -6,6 +6,10 @@ from app.definitions import ROOT_DIR
 def read_pdf(filename="demo.pdf"):
     filepath = ROOT_DIR / "files" / filename
 
-    reader = PdfReader(filepath)
-    page = reader.pages[0]
-    return page.extract_text()
+    try:
+        reader = PdfReader(filepath)
+        page = reader.pages[0]
+        return page.extract_text()
+    except Exception as e:
+        print("An exception occurred:", e)
+        return ''
